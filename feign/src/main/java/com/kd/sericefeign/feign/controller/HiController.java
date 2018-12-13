3409873e720aeb108469c69eee6704a5b7068b1f
yanweirui02 <851217001@qq.com>
2018-12-13 15:10:46 +0800

    新增feign   demo

---
+++ feign/src/main/java/com/kd/sericefeign/feign/controller/HiController.java
@@ -0,0 +1,24 @@
+package com.kd.sericefeign.feign.controller;
+
+import com.kd.sericefeign.feign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wryan
+ * @version 1.0
+ * @create 2018-12-13 14:42
+ **/
+@RestController
+public class HiController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+
+}
